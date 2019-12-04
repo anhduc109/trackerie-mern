5731e4a3a51e92694bb8da5be5883e101ff3dd9c
Duc Pham <ducpham1098@gmail.com>
2019-12-04 09:02:30 +0700

    production build

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,12 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.use(cors());
 app.use(express.json());
 
@@ -19,12 +25,6 @@ connection.once("open", () => {
 
 const exercisesRouter = require("./routes/exercises");
 const usersRouter = require("./routes/users");
-
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
