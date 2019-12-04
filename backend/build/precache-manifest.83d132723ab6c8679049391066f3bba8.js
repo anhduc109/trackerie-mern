ce004b388b84216dd31ca03674a53ec0f6029d96
Duc Pham <ducpham1098@gmail.com>
2019-12-04 22:02:43 +0700

    production build

---
+++ backend/build/precache-manifest.83d132723ab6c8679049391066f3bba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb9ff0acd59e5f1a07a146e8cf70576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d20887efb2fd0eb3a2bf",
+    "url": "/static/css/2.a945d843.chunk.css"
+  },
+  {
+    "revision": "9f6624edcc9a4e29379b",
+    "url": "/static/css/main.ceb1ddda.chunk.css"
+  },
+  {
+    "revision": "d20887efb2fd0eb3a2bf",
+    "url": "/static/js/2.2c892e60.chunk.js"
+  },
+  {
+    "revision": "9f6624edcc9a4e29379b",
+    "url": "/static/js/main.2b49c8bd.chunk.js"
+  },
+  {
+    "revision": "a7887ab8b0e0ba0d7b96",
+    "url": "/static/js/runtime-main.d0589027.js"
+  }
+]);
