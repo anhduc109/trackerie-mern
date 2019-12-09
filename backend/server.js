6ce90144bbbb353b98514e12628c47dd04634e2f
Duc Pham <ducpham1098@gmail.com>
2019-12-09 22:03:44 +0700

    production build

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,12 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build", "index.html"));
+// });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
 });
 
 app.use(cors());
