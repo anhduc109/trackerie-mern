82a071d691593e590e0feef4b43015cd37f0ca10
Duc Pham <ducpham1098@gmail.com>
2019-12-09 22:53:44 +0700

    production build

--- backend/server.js
+++ backend/server.js
@@ -24,18 +24,20 @@ connection.once("open", () => {
 const exercisesRouter = require("./routes/exercises");
 const usersRouter = require("./routes/users");
 
-app.use("/exercises", exercisesRouter);
-app.use("/users", usersRouter);
+if (process.env.NODE_ENV === "production") {
+  app.use("/exercises", exercisesRouter);
+  app.use("/users", usersRouter);
 
-app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "../build")));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../build", "index.html"));
-// });
+  // app.get("/", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../build", "index.html"));
+  // });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
