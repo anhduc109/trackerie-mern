e746c940348022bb9b54897331275e09a5132544
Duc Pham <ducpham1098@gmail.com>
2019-12-09 23:18:41 +0700

    production build

--- backend/server.js
+++ backend/server.js
@@ -28,14 +28,14 @@ if (process.env.NODE_ENV === "production") {
   app.use("/exercises", exercisesRouter);
   app.use("/users", usersRouter);
 
-  app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "/build")));
 
   // app.get("/", (req, res) => {
   //   res.sendFile(path.join(__dirname, "../build", "index.html"));
   // });
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/build", "index.html"));
   });
 }
 
